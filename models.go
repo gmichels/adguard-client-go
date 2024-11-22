6a25dbd86937297d37aa88421dbbb147437cce2f
Gustavo Michels <gmichels@users.noreply.github.com>
2024-11-22 16:12:04 -0600

    feat: add `ecosia` to the list of Safe Search services (#35)

--- models.go
+++ models.go
@@ -178,6 +178,7 @@ type SafeSearchConfig struct {
 	Enabled    bool `json:"enabled"`
 	Bing       bool `json:"bing"`
 	Duckduckgo bool `json:"duckduckgo"`
+	Ecosia     bool `json:"ecosia"`
 	Google     bool `json:"google"`
 	Pixabay    bool `json:"pixabay"`
 	Yandex     bool `json:"yandex"`
