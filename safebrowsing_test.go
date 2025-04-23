a3a8bb8a4ae583c197aea3bb4163f8d2148b478b
Gustavo Michels <gustavomichels@icloud.com>
2025-04-23 13:34:14 -0500

    add test suite for safebrowsing

---
+++ safebrowsing_test.go
@@ -0,0 +1,59 @@
+package adguard
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+// Test SafeBrowsingEnable
+func TestSafeBrowsingEnable(t *testing.T) {
+	adg := createADG()
+
+	// call the method
+	err := adg.SafeBrowsingEnable()
+
+	// assertions
+	assert.NoError(t, err)
+
+	// verify the changes by calling SafeBrowsingStatus
+	result, err := adg.SafeBrowsingStatus()
+	assert.NoError(t, err)
+	assert.NotNil(t, result)
+	// ensure safebrowsing is enabled
+	assert.Equal(t, true, result.Enabled)
+}
+
+// Test SafeBrowsingDisable
+func TestSafeBrowsingDisable(t *testing.T) {
+	adg := createADG()
+
+	// call the method
+	err := adg.SafeBrowsingDisable()
+
+	// assertions
+	assert.NoError(t, err)
+
+	// verify the changes by calling SafeBrowsingStatus
+	result, err := adg.SafeBrowsingStatus()
+	assert.NoError(t, err)
+	assert.NotNil(t, result)
+	// ensure safebrowsing is disabled
+	assert.Equal(t, false, result.Enabled)
+}
+
+// Test SafeBrowsingStatus
+func TestSafeBrowsingStatus(t *testing.T) {
+	adg := createADG()
+
+	// call the method
+	result, err := adg.SafeBrowsingStatus()
+
+	// assertions
+	assert.NoError(t, err)
+	assert.NotNil(t, result)
+	// ensure the status is valid (either true or false)
+	assert.Condition(t, func() bool {
+		return result.Enabled == true || result.Enabled == false
+	})
+}
